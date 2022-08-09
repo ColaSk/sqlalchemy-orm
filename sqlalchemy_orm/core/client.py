cb82546d22f1fe3ef350af11091474e738168b22
sk <ldu_sunkaixuan@163.com>
2022-08-09 23:55:29 +0800

    代码迁移，添加新的特性，需要研究一下model的生成原理

--- sqlalchemy_orm/core/client.py
+++ sqlalchemy_orm/core/client.py
@@ -12,144 +12,6 @@ from .model import Model
 from .session import SignallingSession
 
 
-# TODO：将被删除
-class SQLAlchemyClient(object):
-
-    def __init__(
-        self,
-        config: dict,
-        query_class: BaseQuery = BaseQuery, 
-        session_options: t.Optional[dict] = None,
-        model_class: Model=Model,
-        metadata=None,
-        engine_options: t.Optional[dict]=None):
-
-        self.connectors = {}
-        
-        self.config = config
-        self.Query = query_class
-        self.session_options = session_options
-        self._engine_option = engine_options
-        self._engine_lock = Lock()
-
-        self.session = self.create_scoped_session(session_options)
-        self.Model = self.make_declarative_base(model_class, metadata)
-
-
-    def create_scoped_session(self, options: t.Optional[dict] = None):
-
-        if options is None:
-            options = {}
-
-        scopefunc = options.pop('scopefunc', _ident_func)
-        options.setdefault('query_cls', self.Query)
-        return scoped_session(
-            self.create_session(options), scopefunc=scopefunc
-        )
-
-    def make_declarative_base(self, model_class, metadata=None):        
-
-        model = declarative_base(
-            cls=model_class,
-            name='Model'
-        )
-
-        if metadata is not None and model.metadata is not metadata:
-            model.metadata = metadata
-        
-        if not getattr(model, 'query_class', None):
-            model.query_class = self.Query
-
-        return model
-
-    def create_session(self, options: dict):
-        return sessionmaker(class_=SignallingSession, db=self, **options)
-
-    @property
-    def engine(self):
-        return self.get_engine()
-
-    def get_engine(self, bind=None):
-
-        with self._engine_lock:
-            connector = self.connectors.get(bind)
-
-            if connector is None:
-                connector = self.make_connector(bind)
-                self.connectors[bind] = connector
-
-            return connector[bind]
-    
-    def get_tables_for_bind(self, bind=None):
-
-        result = []
-        for table in iter(self.Model.metadata.tables.values()):
-            if table.info.get('bind_key') == bind:
-                result.append(table)
-        return result
-
-    def get_binds(self):
-        binds = [None] + list(self.config.get('SQLALCHEMY_BINDS') or ())
-        retval = {}
-        for bind in binds:
-            engine = self.get_engine(bind)
-            tables = self.get_tables_for_bind(bind)
-            retval.update(dict((table, engine) for table in tables))
-        return retval
-    
-    def get_uri(self, bind=None):
-        if bind is None:
-            return self.config['SQLALCHEMY_DATABASE_URI']
-        binds = self.config.get('SQLALCHEMY_BINDS') or ()
-        return binds[bind]
-
-    def make_connector(self, bind=None):
-        """Creates the connector for a given state and bind."""
-        uri = self.get_uri(bind)
-        echo = self.config.get('SQLALCHEMY_ECHO', None)
-        sa_url = make_url(uri)
-        sa_url, options = self.get_options(sa_url, echo)
-        engine = self.create_engine(sa_url, options)
-
-        return engine
-
-    def get_options(self, sa_url, echo):
-        options = {}
-
-        options = self.apply_pool_defaults(options)
-        sa_url, options = self.apply_driver_hacks(sa_url, options)
-
-        if echo:
-            options['echo'] = echo
-
-        # Give the config options set by a developer explicitly priority
-        # over decisions FSA makes.
-        options.update(self.config['SQLALCHEMY_ENGINE_OPTIONS'])
-
-        # Give options set in SQLAlchemy.__init__() ultimate priority
-        options.update(self._sa._engine_options)
-
-        return sa_url, options
-
-    def apply_pool_defaults(self, options):
-
-        def _setdefault(optionkey, configkey):
-            value = self.config.get(configkey, None)
-            if value is not None:
-                options[optionkey] = value
-        _setdefault('pool_size', 'SQLALCHEMY_POOL_SIZE')
-        _setdefault('pool_timeout', 'SQLALCHEMY_POOL_TIMEOUT')
-        _setdefault('pool_recycle', 'SQLALCHEMY_POOL_RECYCLE')
-        _setdefault('max_overflow', 'SQLALCHEMY_MAX_OVERFLOW')
-        return options
-
-
-    def create_engine(self, sa_url, engine_opts):
-        return sqlalchemy.create_engine(sa_url, **engine_opts)
-
-
-# 迁移到此类
-
 class MySQLAlchemy(object):
 
     """
@@ -162,23 +24,29 @@ class MySQLAlchemy(object):
         self,
         config: t.Optional[dict] = None,
         session_options: t.Optional[dict] = None,
-        engine_options: t.Optional[dict]=None):
+        engine_options: t.Optional[dict]=None,
+        query_class: BaseQuery = BaseQuery,
+        model_class: Model=Model,
+        metadata=None):
         
         # 所有连接对象
         self._connectors = {}
 
-        self._config = config if config else {}
-
+        self._config = config or {}
         self._session_options = session_options
         self._engine_options = engine_options
+        self.Query = query_class
+        self.model_class = model_class
+        self.metadata = metadata
 
         self._engine_lock = Lock()
 
         self.check_config()
 
         self.session = self.create_scoped_session(session_options)
-        self.Model = self.make_declarative_base()
-
+        self.Model = self.make_declarative_base(model_class, metadata)
+        
+        # TODO: include_sqlalchemy
     
     def check_config(self):
         """check config"""
@@ -204,13 +72,13 @@ class MySQLAlchemy(object):
         self._config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', None)
         self._config.setdefault('SQLALCHEMY_ENGINE_OPTIONS', {})
 
-    
     def create_scoped_session(self, options: t.Optional[dict] = None):
 
         if options is None:
             options = {}
 
         scopefunc = options.pop('scopefunc', _ident_func)
+        options.setdefault('query_cls', self.Query)
         return scoped_session(
             self.create_session(options), scopefunc=scopefunc
         )
@@ -223,13 +91,27 @@ class MySQLAlchemy(object):
         return sessionmaker(class_=SignallingSession, db=self, **options)
 
     def get_binds(self):
-        # 获取多数据库定义
-        return
+        """Returns a dictionary with a table->engine mapping.
+        """
+        binds = [None] + list(self._config.get('SQLALCHEMY_BINDS') or ())
+        retval = {}
+        for bind in binds:
+            engine = self.get_engine(bind)
+            tables = self.get_tables_for_bind(bind)
+            retval.update(dict((table, engine) for table in tables))
+        return retval
+    
+    def get_tables_for_bind(self, bind=None):
 
-    def get_engine(self, bind=None):
+        result = []
+        for table in iter(self.Model.metadata.tables.values()):
+            if table.info.get('bind_key') == bind:
+                result.append(table)
+        return result
 
+    def get_engine(self, bind: t.Optional[str] = None):
+        """获取engine"""
         with self._engine_lock:
-
             connector = self._connectors.get(bind)
 
             if connector is None:
@@ -237,36 +119,90 @@ class MySQLAlchemy(object):
                 self._connectors[bind] = connector
 
             return connector
-
-    
+ 
     def get_uri(self, bind=None):
+        """获取uri"""
         if bind is None:
-            return self._config['url']
+            return self._config['SQLALCHEMY_DATABASE_URI']
         binds = self._config.get('SQLALCHEMY_BINDS') or ()
         return binds[bind]
 
-    
     def make_connector(self, bind=None):
-        """Creates the connector for a given state and bind."""
+        """Creates the connector for a given state and bind.
+        1. 获取指定bind的uri
+        2. 生成url
+        3. 获取options
+        4. 生成 engine
+        """
+
         uri = self.get_uri(bind)
-        options = {
-            "echo": True
-        } # TODO：根据url定义具体的options，针对不用数据库options存在支持与不支持的问题
-        engine = self.create_engine(uri, options)
+        echo = self._config.get('SQLALCHEMY_ECHO', None)
+        url = make_url(uri)
+        url, options = self.get_options(url, echo)
+        engine = self.create_engine(url, options)
 
         return engine
+ 
+    def get_options(self, sa_url, echo):
+        options = {}
+
+        options = self.apply_pool_defaults(options)
+        sa_url, options = self.apply_driver_hacks(sa_url, options)
+
+        if echo:
+            options['echo'] = echo
+
+        # Give the config options set by a developer explicitly priority
+        # over decisions FSA makes.
+        options.update(self._config['SQLALCHEMY_ENGINE_OPTIONS'])
+
+        # Give options set in SQLAlchemy.__init__() ultimate priority
+        options.update(self._engine_options)
+
+        return sa_url, options
+
+    def apply_pool_defaults(self, options: dict):
+
+        def _setdefault(optionkey, configkey):
+            """获取配置项中的配置转化为制定函数入参"""
+            value = self._config.get(configkey, None)
+            if value is not None:
+                options[optionkey] = value
+
+        _setdefault('pool_size', 'SQLALCHEMY_POOL_SIZE')
+        _setdefault('pool_timeout', 'SQLALCHEMY_POOL_TIMEOUT')
+        _setdefault('pool_recycle', 'SQLALCHEMY_POOL_RECYCLE')
+        _setdefault('max_overflow', 'SQLALCHEMY_MAX_OVERFLOW')
+
+        return options
+
+    def apply_driver_hacks(self, url, options: dict):
+        """This method is called before engine creation and used to inject
+        driver specific hacks into the options.  The `options` parameter is
+        a dictionary of keyword arguments that will then be used to call
+        the :func:`sqlalchemy.create_engine` function.
+        TODO: 完善
+        """
+        return url, options
 
     def create_engine(self, sa_url, engine_opts):
         return sqlalchemy.create_engine(sa_url, **engine_opts)
 
-    def make_declarative_base(self): 
+    def make_declarative_base(self, model_class, metadata=None): 
 
         model = declarative_base(
-            bind=self.engine,
+            cls=model_class,
             name='Model'
         )
 
+        if metadata is not None and model.metadata is not metadata:
+            model.metadata = metadata
+        
+        if not getattr(model, 'query_class', None):
+            model.query_class = self.Query
+
         return model
+
 
 
     
--- sqlalchemy_orm/core/session.py
+++ sqlalchemy_orm/core/session.py
@@ -12,3 +12,22 @@ class SignallingSession(SessionBase):
             self, autocommit=autocommit, autoflush=autoflush,
             bind=bind, binds=binds, **options
         )
+
+    def get_bind(self, mapper=None, clause=None):
+        """Return the engine or connection for a given model or
+        table, using the ``__bind_key__`` if it is set.
+        """
+        # mapper is None if someone tries to just get a connection
+        if mapper is not None:
+            try:
+                # SA >= 1.3
+                persist_selectable = mapper.persist_selectable
+            except AttributeError:
+                # SA < 1.3
+                persist_selectable = mapper.mapped_table
+
+            info = getattr(persist_selectable, 'info', {})
+            bind_key = info.get('bind_key')
+            if bind_key is not None:
+                return self.db.get_engine(bind=bind_key)
+        return SessionBase.get_bind(self, mapper, clause)
